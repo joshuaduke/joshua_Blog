219cb789a9f04314156b74672aee62a0ec0d6288
Joshua Duke <dukejoshuae@gmail.com>
2020-02-20 10:11:44 -0500

    Database set up

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-let express = require('express');
-let app = express();
-let mongoose = require("mongoose");
-let bodyParser = require('body-parser');
+let methodOverride  = require('method-override');
+let bodyParser      = require('body-parser');
+let mongoose        = require("mongoose");
+let express         = require('express');
+let app             = express();
 
 app.set("view engine", "ejs"); // ejs files
+mongoose.connect("mongodb://localhost/JOSHUA_BLOG" ,{ useNewUrlParser: true }); //mongoose connection
+app.use(express.static("public")); // To use css stylesheets
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method")); //look for _method take whatever its equal to and treat that request as a put or delete request
+
+//database schema
+
+/*
+  Title
+  Image
+  Body
+  Tags
+*/
+let blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now},
+  tags: []
+});
+
+let Blog = mongoose.model("Blog", blogSchema);
+
 
 app.get("/", (req, res)=>{
   res.send("This is the homepage");
